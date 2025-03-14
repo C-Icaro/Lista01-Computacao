4332776705d148d05560dd5e5b9e26f11088bbf1
Carlos Icaro <carlos.paiva@sou.inteli.edu.br>
2025-03-14 01:04:13 -0300

    Lista Finalizada (+Q10)

--- questões.js
+++ questões.js
@@ -97,28 +97,37 @@ function somaArray(numeros) {
 console.log("Valor da soma do dobro do array:", somaArray([1, 2, 3, 4]));//Console log para exibir o retorno da função 
 
 //10ºQuestão
-class Produto{
-    constructor(nome, preço){
-        this.nome = nome;
+class Produto{//Classe pai 
+    constructor(nome, preço){//Constructor pai
+        this.nome = nome;//Definição de propriedades/atributos
         this.preço = preço;
     }
 
-    calcularDesconto(){
-        this.preço = this.preço * 0.9;
-        console.log(`Preço do livro com 10% de desconto R$ ${this.preço}.`);
+    calcularDesconto(){//Método calcular desconto pai
+        this.preço = this.preço * 0.9;//Linha para calcular e aplicar o desconto
+        console.log(`Preço do livro com 10% de desconto R$ ${this.preço}.`);//Console log de visualização e controle
     }
 }
 
-class Livro extends Produto{
-    constructor(nome, preço){
+class Livro extends Produto{//Classe filha
+    constructor(nome, preço){//Constructor filho
         super(nome, preço);
     }
 
-    calcularDesconto(){
-        this.preço = this.preço * 0.8;
-        console.log(`Preço do livro com 20% de desconto R$ ${this.preço}.`);
+    calcularDesconto(){//Método calcular desconto filho
+        this.preço = this.preço * 0.8;//Linha para calcular e aplicar o desconto
+        console.log(`Preço do livro com 20% de desconto R$ ${this.preço}.`);//Console log de visualização e controle
     }
 }
 
-const L1984 = new Livro("1984", 100);
-L1984.calcularDesconto();
+const L1984 = new Livro("1984", 100);//Criação de um objeto na classe filha
+L1984.calcularDesconto();//Chamamento do método calcular desconto filho
+
+/*
+Explicação:
+A classe Produto é criada e cria as propriedades nome e preço, além do método calcularDesconto(). A classe Livro extende a classe Produto e herda seus atributos e métodos. 
+A palavra chave super no classe Livro chama o constructor da classe pai e solicita acesso aos atributos elencados (herança).
+A modificação do método ocorre por sobrescrita de método, ou seja, estamos criando um método igual mas com comportamento diferente, estamos aplicando o polimorfismo.
+Podemos ter certeza que a sobrescrita é responsável pela modificação do método quando retiramos essa modificação da classe Livro. Sem nenhuma sobrescrita é chamado o método pai
+presente na classe Produto, tendo como retorno no console log o valor R$90 (referente à 10% do valor do produto).
+*/
